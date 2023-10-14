12961eac793e898e88bbda1c54cc4dc109074116
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-13 21:35:37 -0400

    project maven using spring boot

---
+++ src/test/java/br/com/emilly/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emilly.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
