87e421094a42fb0df06b25eb913f5a9bcd297d52
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-10-14 18:29:49 -0400

    Configure H2 database properties for Spring Boot application

--- src/main/java/br/com/emilly/todolist/user/UserModel.java
+++ src/main/java/br/com/emilly/todolist/user/UserModel.java
@@ -1,11 +1,13 @@
 package br.com.emilly.todolist.user;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+    
     private String username;
     private String name;
     private String password;
-
 }
